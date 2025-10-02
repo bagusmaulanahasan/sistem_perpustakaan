41c3e16a92743cf7783e26f7d0400cbb22ba0d06
Bagus Maulana <bagusmaulana0320@gmail.com>
2025-10-02 15:36:00 +0700

    doc: update export db

--- db_pinjam_buku.sql
+++ db_pinjam_buku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 29, 2025 at 10:46 AM
+-- Generation Time: Oct 02, 2025 at 08:34 AM
 -- Server version: 8.0.42-0ubuntu0.24.04.2
 -- PHP Version: 8.3.6
 
@@ -44,11 +44,18 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `title`, `author`, `publisher`, `publication_year`, `stock`, `category_id`, `cover_image_url`, `created_at`) VALUES
-(1, 'Petualangan di Dunia Fantasi', 'Rina Wulandari', 'Gramedia', '2020', 9, 1, 'covers/fantasi.jpg', '2025-09-29 09:59:43'),
-(2, 'Belajar Python untuk Pemula', 'Andi Nugroho', 'Informatika Press', '2022', 4, 3, 'covers/python.jpg', '2025-09-29 09:59:43'),
-(3, 'Sejarah Nusantara', 'Dewi Kartika', 'Kompas', '2018', 7, 4, 'covers/nusantara.jpg', '2025-09-29 09:59:43'),
-(4, 'Psikologi Anak Modern', 'Dr. Siti Aminah', 'Mizan', '2021', 3, 2, 'covers/psikologi.jpg', '2025-09-29 09:59:43'),
-(5, 'Dongeng Sebelum Tidur', 'Tono Prasetyo', 'Tiga Serangkai', '2019', 12, 5, 'covers/dongeng.jpg', '2025-09-29 09:59:43');
+(1, 'Petualangan di Dunia Fantasi', 'Rina Wulandari', 'Gramedia', '2020', 9, 1, '/uploads/covers/cover_image-1759306877151-131979542.jpg', '2025-09-29 09:59:43'),
+(2, 'Belajar Python untuk Pemula', 'Andi Nugroho', 'Informatika Press', '2022', 9, 3, '/uploads/covers/cover_image-1759372504755-877040319.jpeg', '2025-09-29 09:59:43'),
+(3, 'Sejarah Nusantara', 'Dewi Kartika', 'Kompas', '2018', 7, 4, '/uploads/covers/cover_image-1759372424570-471149537.jpg', '2025-09-29 09:59:43'),
+(4, 'Psikologi Anak Modern', 'Dr. Siti Aminah', 'Mizan', '2021', 3, 2, '/uploads/covers/cover_image-1759372391161-967245093.png', '2025-09-29 09:59:43'),
+(5, 'Dongeng Horror Sebelum Tidur', 'Tono Prasetyo', 'Tiga Serangkai', '2018', 15, 5, '/uploads/covers/cover_image-1759307371890-369389190.png', '2025-09-29 09:59:43'),
+(6, 'Tips & Trik Bertahan Hidup di Universitas Pamulang', 'NotNeverMan', 'PerpusUnpam', '2024', 7, 4, '/uploads/covers/cover_image-1759306754037-52500730.png', '2025-10-01 08:05:21'),
+(7, 'Petualangan Si Kancil', 'R. A. Kosasih', 'Gramedia Kids', '2018', 14, 1, '/uploads/covers/cover_image-1759372336946-287319436.jpg', '2025-10-01 10:33:51'),
+(10, 'Dunia Tanpa Batas', 'Mira Lesmana', 'Bentang Pustaka', '2021', 10, 3, '/uploads/covers/cover_image-1759372230641-18385117.jpg', '2025-10-01 10:33:51'),
+(11, 'Fakta Menakjubkan Tentang Otak', 'Prof. Nina Kartika', 'IlmuPopuler', '2019', 7, 2, '/uploads/covers/cover_image-1759372283264-321657569.jpg', '2025-10-01 10:33:51'),
+(12, 'Robot dan Anak Ajaib', 'T. Arief Pratama', 'AnakCerdas', '2023', 13, 1, '/uploads/covers/cover_image-1759360779846-605071448.jpeg', '2025-10-01 10:33:51'),
+(13, 'Keindahan dalam sebuah Furniture', 'Lowsen Hayne', 'FurMaz', '2014', 8, 2, '/uploads/covers/cover_image-1759360829672-792749098.jpg', '2025-10-01 23:20:29'),
+(14, 'The King Savannah', 'Drawd Edersoon', 'AnimalFams', '1978', 11, 2, '/uploads/covers/cover_image-1759392896204-380041856.jpg', '2025-10-02 08:14:56');
 
 -- --------------------------------------------------------
 
@@ -70,8 +77,17 @@ CREATE TABLE `borrowings` (
 --
 
 INSERT INTO `borrowings` (`id`, `user_id`, `book_id`, `borrow_date`, `due_date`, `return_date`) VALUES
-(1, 1, 1, '2025-09-29', '2025-10-13', NULL),
-(2, 1, 2, '2025-09-29', '2025-10-13', NULL);
+(1, 1, 1, '2025-09-29', '2025-10-13', '2025-10-01'),
+(2, 1, 2, '2025-09-29', '2025-10-13', NULL),
+(3, 1, 6, '2025-10-01', '2025-10-15', NULL),
+(6, 1, 5, '2025-10-02', '2025-10-16', '2025-10-02'),
+(7, 4, 14, '2025-10-02', '2025-10-16', NULL),
+(8, 4, 6, '2025-10-02', '2025-10-16', '2025-10-02'),
+(9, 4, 1, '2025-10-02', '2025-10-16', NULL),
+(10, 5, 12, '2025-10-02', '2025-10-16', '2025-10-02'),
+(11, 5, 3, '2025-10-02', '2025-10-16', '2025-10-02'),
+(12, 5, 11, '2025-10-02', '2025-10-16', '2025-10-02'),
+(13, 5, 10, '2025-10-02', '2025-10-16', NULL);
 
 -- --------------------------------------------------------
 
@@ -91,6 +107,7 @@ CREATE TABLE `categories` (
 INSERT INTO `categories` (`id`, `name`) VALUES
 (5, 'Anak-anak'),
 (1, 'Fiksi'),
+(6, 'Matematika'),
 (2, 'Non-Fiksi'),
 (4, 'Sejarah'),
 (3, 'Teknologi');
@@ -115,8 +132,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `created_at`) VALUES
-(1, 'Bagus Maulana', 'bagusmaulana320@gmail.com', '$2b$12$3Ph2HpXmkXoPl9x2h8VuGu0Lzzj1huXm29oUDhq1bKKG1pN/TB2bi', 'anggota', '2025-09-29 09:04:29'),
-(2, 'admin', 'admin@gmail.com', '$2b$12$matnLhOkCzoy6Ve2oi91xeq4gs/2AbcqsjdbWt9K39aVPwGODxmda', 'admin', '2025-09-29 09:13:01');
+(1, 'Bagus Maulana', 'bagusmaulana320@gmail.com', '$2b$12$3II1xW9Fk0hKsK.bYo0cVeNZlQHDD.np7gKppzpJwaTpACVXuybaa', 'anggota', '2025-09-29 09:04:29'),
+(2, 'admin', 'admin@gmail.com', '$2b$12$matnLhOkCzoy6Ve2oi91xeq4gs/2AbcqsjdbWt9K39aVPwGODxmda', 'admin', '2025-09-29 09:13:01'),
+(3, 'testing123', 'testing123@gmail.com', '$2b$12$Q0HqBGxBLnFX.9RT75FctuhnALOrCcuyEr3tVPjNxXejIFeHYxE32', 'anggota', '2025-10-02 04:31:47'),
+(4, 'Reysa', 'reysa@gmail.com', '$2b$12$zrTeACpqc3O2B57r6khYZu9RC1Rf.3X2a1RDYNfrJINNWr2xoPYmm', 'anggota', '2025-10-02 08:30:08'),
+(5, 'davina', 'davina@gmail.com', '$2b$12$Eq.Yb6revX8avwGXJ6l6E.R/a4vrSDtkkYgYWKmkQFgC3335uCnoa', 'anggota', '2025-10-02 08:31:30');
 
 -- --------------------------------------------------------
 
@@ -138,7 +158,16 @@ CREATE TABLE `wishlists` (
 INSERT INTO `wishlists` (`id`, `user_id`, `book_id`, `created_at`) VALUES
 (4, 1, 4, '2025-09-29 10:04:42'),
 (5, 1, 3, '2025-09-29 10:11:35'),
-(8, 1, 5, '2025-09-29 10:18:42');
+(8, 1, 5, '2025-09-29 10:18:42'),
+(11, 1, 6, '2025-10-01 09:22:27'),
+(12, 1, 2, '2025-10-01 10:49:18'),
+(14, 4, 5, '2025-10-02 08:30:22'),
+(15, 4, 13, '2025-10-02 08:30:28'),
+(16, 4, 12, '2025-10-02 08:30:33'),
+(17, 4, 14, '2025-10-02 08:30:40'),
+(18, 5, 2, '2025-10-02 08:31:42'),
+(19, 5, 6, '2025-10-02 08:31:47'),
+(21, 5, 10, '2025-10-02 08:32:39');
 
 --
 -- Indexes for dumped tables
@@ -189,31 +218,31 @@ ALTER TABLE `wishlists`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `borrowings`
 --
 ALTER TABLE `borrowings`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `wishlists`
 --
 ALTER TABLE `wishlists`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
